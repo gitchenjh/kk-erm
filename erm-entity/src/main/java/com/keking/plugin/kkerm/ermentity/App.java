f5ee802be59a2d22c2ab13c09e45997295aca973
陈精华 <842761733@qq.com>
2018-10-23 15:24:21 +0800

    初始提交

---
+++ erm-entity/src/main/java/com/keking/plugin/kkerm/ermentity/App.java
@@ -0,0 +1,12 @@
+package com.keking.plugin.kkerm.ermentity;
+
+/**
+ * @auther: chenjh
+ * @time: 2018/10/23 15:02
+ * @description
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println( "Hello World!" );
+    }
+}
